61331b22a8514336bc8cd02f93fa53503b07b654
Logan Miller <info@cascadeonlinedesign.com>
2026-06-03 17:41:22 +0000

    add: initial database migration for all Payload collections

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260603_174048 from './20260603_174048';
+
+export const migrations = [
+  {
+    up: migration_20260603_174048.up,
+    down: migration_20260603_174048.down,
+    name: '20260603_174048'
+  },
+];
